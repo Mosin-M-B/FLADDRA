323f1104aa88b74f2d301b71dae984fffe5d09af
Mosin Balsing <119176708+Mosin-M-B@users.noreply.github.com>
2024-07-02 15:32:17 +0530

    Update Data.jsx add btn in Data

--- Client/src/Pages/Data.jsx
+++ Client/src/Pages/Data.jsx
@@ -3,6 +3,11 @@ export const Data = () =>{
         <>
         <h1>Data</h1>
         <p>Welcome to the Data Solutions page!</p>
+             <button id='infosysbtn' className="clickinfosys">
+        <a href="https://www.infosysbpm.com/contact/apac-bpm.html#india'" target="blank">
+        Infosys BPM
+        </a>
+        </button>
         </>
     )
 }
