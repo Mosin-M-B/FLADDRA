974dd3237a4a9b93bd73ee8a0aee67af351cb806
Mosin Balsing <119176708+Mosin-M-B@users.noreply.github.com>
2024-07-08 12:50:46 +0530

    Update AI.jsx add API

--- Client/src/Pages/AI.jsx
+++ Client/src/Pages/AI.jsx
@@ -1,4 +1,16 @@
 export const AI = () => {
+ const push = async () => {
+  try {
+    const response = await fetch("https://umairrinde.pythonanywhere.com/api/script/key=button_click");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
   return (
     <div>
       <h1>AI Solutions</h1>
